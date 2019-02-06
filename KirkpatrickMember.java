b161fb7e21d497f375d79e665f0d0f0f90bbdb02
Riley Kirkpatrick <kirkpatrickr@moravian.edu>
2019-02-06 12:05:46 -0500

    Created KirkpatrickMember class

---
+++ KirkpatrickMember.java
@@ -0,0 +1,19 @@
+package Project00;
+
+import java.util.*;
+
+public class KirkpatrickMember
+{
+    String memberName;
+
+    public KirkpatrickMember(String name)
+    {
+        memberName = name;
+    }
+
+    public String getMemberName()
+    {
+        return memberName;
+    }
+
+}
